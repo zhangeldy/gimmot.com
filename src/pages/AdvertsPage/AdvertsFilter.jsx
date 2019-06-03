4e32ae708c5feba3060e4070e0e05771ef8f4b89
Jangle <n.zhangeldy@gmail.com>
2019-06-03 13:48:25 +0600

    ---

--- src/pages/AdvertsPage/AdvertsFilter.jsx
+++ src/pages/AdvertsPage/AdvertsFilter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import StarIcon from "@material-ui/icons/GradeOutlined";
 import FilterIcon from "@material-ui/icons/FilterList";
 import withTranslation from "../../_hoc/withTranslation";
@@ -7,25 +7,21 @@ import Button from "../../_ui/Button/Button";
 import IconButton from "../../_ui/Button/IconButton";
 
 function AdvertsFilter({ t }) {
-  const [isOpen, openBlock] = useState(false);
-
   return (
     <Filter className="paper my2 p1">
       <Button text={t("advertsFilter_addAdvert")} size="small" />
-      {!isOpen && (
-        <div className="right">
-          <IconButton
-            tooltip={t("advertsFilter_favorites")}
-            style={{ padding: 5, marginLeft: 5 }}
-            icon={<StarIcon fontSize="small" />}
-          />
-          <IconButton
-            tooltip={t("advertsFilter_filter")}
-            style={{ padding: 5, marginLeft: 5 }}
-            icon={<FilterIcon fontSize="small" />}
-          />
-        </div>
-      )}
+      <div className="right">
+        <IconButton
+          tooltip={t("advertsFilter_favorites")}
+          style={{ padding: 5, marginLeft: 5 }}
+          icon={<StarIcon fontSize="small" />}
+        />
+        <IconButton
+          tooltip={t("advertsFilter_filter")}
+          style={{ padding: 5, marginLeft: 5 }}
+          icon={<FilterIcon fontSize="small" />}
+        />
+      </div>
     </Filter>
   );
 }
