4e968f94fcaeaef33ad9778e2e18357f8f288ff9
Jangle <n.zhangeldy@gmail.com>
2020-02-11 18:31:48 +0600

    restructure

--- src/utils/errorMsg.js
+++
@@ -1,9 +0,0 @@
-export function errorMsg(name, { errors, touched, submitCount }) {
-  let errorMsg = "";
-  if (submitCount && errors[name]) {
-    errorMsg = errors[name];
-  } else if (touched[name] && errors[name]) {
-    errorMsg = errors[name];
-  }
-  return errorMsg;
-}
